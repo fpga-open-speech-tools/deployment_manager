455f564fca2929af9b443dbed50b8cd61ea172b9
Trevor Vannoy <vannoy@flatearthinc.com>
2020-05-21 19:43:32 -0600

    determine register paths when loading in the Linker at startup.
    On start, an object is created that maps module/link names from the linker
    with the paths to the register file in sysfs. The major number for each
    device in the linker is obtained via a unix one-liner

--- service.js
+++ service.js
@@ -1,3 +1,5 @@
+// 'use strict';
+
 const url = require('url');
 const fs = require('fs');
 
@@ -8,31 +10,74 @@ const downloadScriptPath = "../utils"
 const configPath = "../config"
 
 
-var cmd;
+var registerPaths = {};
 
 var CommandObject;
 var progress = JSON.parse('{"name": "", "progress": 0, "status": ""}');
 // UIrawdata = fs.readFileSync('UI.json');
 // UIObject = JSON.parse(UIrawdata);
 
-exports.Init = function(){
+exports.Init = function () {
     return;
 }
 
 
-findEchoFileFromLinker = function(cmdObj){
-    var targetDriver = LinkerObject[cmdObj.module];
-    //var targetName = cmdObj.module.ToString();
-    var links =  targetDriver['links'];
-    var filepath = deviceLocation 
-    + fe + cmdObj.module.trim() + '_' + targetDriver.majorNo.trim()
-    + fe + cmdObj.module.trim() + '_' + targetDriver.majorNo.trim()
-    + links[cmdObj.link].trim();
-    //console.log('Command Prepared.' + filepath);
+
+function createDevicePath (moduleName, majorNumber) {
+    console.log("hi");
+    // var targetDriver = LinkerObject[cmdObj.module];
+    // //var targetName = cmdObj.module.ToString();
+    // var links = targetDriver['links'];
+    // console.log('asdf');
+    // var majorNo = findMajorNumber(cmbObj.module
+    const filepath = deviceLocation
+        + fe + moduleName.trim() + '_' + majorNumber
+        + fe + moduleName.trim() + '_' + majorNumber;
+    console.log('Command Prepared.' + filepath);
     return filepath;
 }
 
-getJsonFromFile = function(filepath){
+function findMajorNumber (moduleName) {
+    // TODO: error handling; if module is not installed, the grep | cut command will return an empty string. We need to make sure that the returned major number is indeed an integer
+    console.log('hello');
+    const { execSync } = require('child_process');
+    const cmd = 'ls /sys/class | grep -P "fe_' + moduleName + '_\\d+" | cut -d _ -f 3';
+    console.log(cmd);
+    const majorNumber = execSync(cmd);
+    console.log(majorNumber)
+    console.log(majorNumber.toString().trim());
+    return majorNumber.toString().trim();
+}
+
+function mapLinkNamesToFilepaths () {
+    registerPaths = {};
+     
+    // TODO: naming consistency between module and device (they refer to the same thing here)
+
+    for (const device in LinkerObject) {
+        if (LinkerObject.hasOwnProperty(device)) {
+            const deviceObject = LinkerObject[device];
+            const links = deviceObject['links'];
+
+            registerPaths[device] = {};
+
+            const majorNumber = findMajorNumber(device);
+            const devicePath = createDevicePath(device, majorNumber);
+
+            for (const link in links) {
+                if (links.hasOwnProperty(link)) {
+                    const registerName = links[link];
+
+                    // XXX: registerName is assumed to have a leading / in the linker json. This should always be the case, but having some error handling would be nice
+                    registerPaths[device][link] = devicePath + registerName;
+                }
+            }
+        }
+    }
+    console.log(registerPaths);
+}
+
+const getJsonFromFile = function (filepath) {
     fs.open;
     // console.log(configPath + '/UI.json')
     // fs.readFile(configPath + '/UI.json','utf-8',(err,data) => {
@@ -41,16 +86,17 @@ getJsonFromFile = function(filepath){
     //     UIObject = JSON.parse(data);
     //     console.log(JSON.stringify(UIObject));
     // });
-    const objFromFile = fs.readFileSync(filepath, 
-        {encoding: 'utf-8', flag: 'r'})
+    const objFromFile = fs.readFileSync(filepath,
+        { encoding: 'utf-8', flag: 'r' })
     fs.close;
 
     return JSON.parse(objFromFile);
 }
 
 var LinkerObject = getJsonFromFile(configPath + '/Linker.json');
+mapLinkNamesToFilepaths();
 
-execCB = function(error, stdout, stderr) {
+const execCB = function (error, stdout, stderr) {
     if (error) {
         console.error(`exec error: ${error}`);
         return;
@@ -58,7 +104,7 @@ execCB = function(error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
 }
-progressCB = function(error, stdout, stderr) {
+const progressCB = function (error, stdout, stderr) {
     if (error) {
         console.error(`exec error: ${error}`);
         return;
@@ -73,42 +119,60 @@ progressCB = function(error, stdout, stderr) {
 }
 
 exports.setCommandRequest = function (req, res) {
-    //console.log('New Command Received.');
-    body = '';
+    console.log('New Command Received.');
+    let body = [];
 
+    console.log('New Command Received.');
     req.on('data', function (chunk) {
-        body += chunk;
+        body.push(chunk);
     });
 
+    console.log('New Command Received.');
     req.on('end', function () {
 
-    try{
-        postBody = JSON.parse(body);
-        CommandObject = postBody;
+    console.log('New Command Received.');
+        try {
+            console.log('asdf');
+            postBody = JSON.parse(body);
+            console.log('asdf');
+            CommandObject = postBody;
+            console.log('asdf');
+            
 
-       // console.log(CommandObject)
+            // console.log(CommandObject)
 
-        var echoFile = findEchoFileFromLinker(CommandObject);
-        //console.log('\nExpected echo path: ', echoFile)
+            // var majorNumber = findMajorNumber(CommandObject.module);
+            // console.log(majorNumber);
+            // var echoFile = findEchoFileFromLinker(CommandObject);
+            // console.log(echoFile);
+            //console.log('\nExpected echo path: ', echoFile)
 
-        var exec = require('child_process').exec;
+            console.log('asdfasdf');
+            console.log(registerPaths);
 
-        // any unix based command
-       var cmdToLaunch =  "echo " + CommandObject.value + " > " + echoFile;
-       //var cmdToLaunch = "echo 12.00 > /sys/class/fe_bitcrusher_248/fe_bitcrusher_248/bits";
+            echoFile = registerPaths[CommandObject.module][CommandObject.link];
 
-        //console.log('Command: ', cmdToLaunch);
+            console.log(echoFile);
 
-        var app = exec(cmdToLaunch, execCB);
-        
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(CommandObject));
+            var exec = require('child_process').exec;
 
-    }
-    catch(error){
-        //Do Nothing ;)
-    }
+            // any unix based command
+            var cmdToLaunch = "echo " + CommandObject.value + " > " + echoFile;
+            //var cmdToLaunch = "echo 12.00 > /sys/class/fe_bitcrusher_248/fe_bitcrusher_248/bits";
+
+            console.log('Command: ', cmdToLaunch);
+
+            var app = exec(cmdToLaunch, execCB);
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(CommandObject));
+
+        }
+        catch (error) {
+            //Do Nothing ;)
+            console.error(error);
+        }
     });
 };
 
@@ -146,49 +210,49 @@ exports.setDownloadRequest = function (req, res) {
 
     req.on('end', function () {
 
-    try{
-        postBody = JSON.parse(body);
-        CommandObject = postBody;
+        try {
+            postBody = JSON.parse(body);
+            CommandObject = postBody;
 
-        // any unix based command
-        var cmdToLaunch =  "./" + downloadScriptPath + "/awsdownload.py -b nih-demos -d " + CommandObject.downloadurl;
-        console.log(cmdToLaunch)
+            // any unix based command
+            var cmdToLaunch = "./" + downloadScriptPath + "/awsdownload.py -b nih-demos -d " + CommandObject.downloadurl;
+            console.log(cmdToLaunch)
 
-        // const { spawn } = require("child_process");
-        var exec = require('child_process').exec;
-        exec(cmdToLaunch, progressCB)
+            // const { spawn } = require("child_process");
+            var exec = require('child_process').exec;
+            exec(cmdToLaunch, progressCB)
 
-        // for await (const data of child.stdout) {
-        //     progress = JSON.parse(data)
-        // }
+            // for await (const data of child.stdout) {
+            //     progress = JSON.parse(data)
+            // }
 
-        // for await (const data of child.close) {
-        //     console.log("python is done")
-        // }
+            // for await (const data of child.close) {
+            //     console.log("python is done")
+            // }
 
-        var projectName = CommandObject.projectname.replace('-','_')
+            var projectName = CommandObject.projectname.replace('-', '_')
 
-        var cmdToLaunch = "./" + downloadScriptPath + "/overlaymgr load " + projectName
-        exec(cmdToLaunch, execCB)
+            var cmdToLaunch = "./" + downloadScriptPath + "/overlaymgr load " + projectName
+            exec(cmdToLaunch, execCB)
 
-        var cmdToLaunch = "./" + downloadScriptPath + "/drivermgr load " + projectName
-        exec(cmdToLaunch,execCB)
+            var cmdToLaunch = "./" + downloadScriptPath + "/drivermgr load " + projectName
+            exec(cmdToLaunch, execCB)
 
-        LinkerObject = getJsonFromFile(configPath + '/Linker.json')
-        
-        
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(CommandObject));
+            LinkerObject = getJsonFromFile(configPath + '/Linker.json')
 
-    }
-    catch(error){
-        //Do Nothing ;)
-    }
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(CommandObject));
+
+        }
+        catch (error) {
+            //Do Nothing ;)
+        }
     });
 };
 
-exports.getDownloadProgress = function(req, res) {
+exports.getDownloadProgress = function (req, res) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(progress));
