17e1384a249315bc92f3a5947024ac4dff4a32fd
Trevor Vannoy <vannoy@flatearthinc.com>
2020-05-26 12:32:55 -0600

    move utility functions into utilties.js and remove some debugging output

--- service.js
+++ service.js
@@ -1,235 +1,47 @@
 // 'use strict';
 
 const url = require('url');
-const fs = require('fs');
+const util = require('./utilities.js');
 
-const deviceLocation = "/sys/class";
-const fe = "/fe_";
-
-const downloadScriptPath = "../utils"
 const configPath = "../config"
 
 var previousProjectName; 
 
 var registerPaths = {};
 
-var downloadProc;
-var LinkerObject;
-
-
 var CommandObject;
 var progress = '{"progress": 0, "status": ""}';
-// UIrawdata = fs.readFileSync('UI.json');
-// UIObject = JSON.parse(UIrawdata);
 
 exports.Init = function () {
     return;
 }
 
 
+var registerPaths = util.loadLinker(configPath);
 
-function createDevicePath (moduleName, majorNumber) {
-    // console.log("hi");
-    // var targetDriver = LinkerObject[cmdObj.module];
-    // //var targetName = cmdObj.module.ToString();
-    // var links = targetDriver['links'];
-    // console.log('asdf');
-    // var majorNo = findMajorNumber(cmbObj.module
-    const filepath = deviceLocation
-        + fe + moduleName.trim() + '_' + majorNumber
-        + fe + moduleName.trim() + '_' + majorNumber;
-    // console.log('Command Prepared.' + filepath);
-    return filepath;
-}
-
-function findMajorNumber (moduleName) {
-    // TODO: error handling; if module is not installed, the grep | cut command will return an empty string. We need to make sure that the returned major number is indeed an integer
-    // console.log('hello');
-    const { execSync } = require('child_process');
-    const cmd = 'ls /sys/class | grep -P "fe_' + moduleName + '_\\d+" | cut -d _ -f 3';
-    // console.log(cmd);
-    const majorNumber = execSync(cmd);
-    // console.log(majorNumber)
-    // console.log(majorNumber.toString().trim());
-    return majorNumber.toString().trim();
-}
-
-function mapLinkNamesToFilepaths () {
-    registerPaths = {};
-     
-    // TODO: naming consistency between module and device (they refer to the same thing here)
-
-    for (const device in LinkerObject) {
-        if (LinkerObject.hasOwnProperty(device)) {
-            const deviceObject = LinkerObject[device];
-            const links = deviceObject['links'];
-
-            registerPaths[device] = {};
-
-            const majorNumber = findMajorNumber(device);
-            const devicePath = createDevicePath(device, majorNumber);
-
-            for (const link in links) {
-                if (links.hasOwnProperty(link)) {
-                    const registerName = links[link];
-
-                    // XXX: registerName is assumed to have a leading / in the linker json. This should always be the case, but having some error handling would be nice
-                    registerPaths[device][link] = devicePath + registerName;
-                }
-            }
-        }
-    }
-    console.log(registerPaths);
-}
-
-function loadLinker() {
-    LinkerObject = getJsonFromFile(configPath + '/Linker.json');
-    mapLinkNamesToFilepaths();
-}
-
-const getJsonFromFile = function (filepath) {
-    fs.open;
-    // console.log(configPath + '/UI.json')
-    // fs.readFile(configPath + '/UI.json','utf-8',(err,data) => {
-    //     // console.log(data)
-    //     if(err){throw err;}
-    //     UIObject = JSON.parse(data);
-    //     console.log(JSON.stringify(UIObject));
-    // });
-    const objFromFile = fs.readFileSync(filepath,
-        { encoding: 'utf-8', flag: 'r' })
-    fs.close;
-
-    return JSON.parse(objFromFile);
-}
-
-function downloadInstallOverlay (s3dir) {
-    return new Promise((resolve, reject) => {
-
-        let errors = [];
-
-        // any unix based command
-        // var cmdToLaunch = downloadScriptPath + "/aws_overlay_installer.py -p json -b nih-demos -d " + CommandObject.downloadurl;
-        // console.log(cmdToLaunch)
-
-        const { spawn } = require("child_process");
-        let downloadProc = spawn('python3', [downloadScriptPath + '/aws_overlay_installer.py', '-e', 'http://127.0.0.1:3355/set-download-progress', '-b', 'nih-demos', '-d', s3dir]);
-
-        downloadProc.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-            if (data.toString().charAt(0) === '{') {
-                // progress = JSON.parse(data);
-                // console.log(data.toString());
-           }
-        });
-
-        downloadProc.stderr.on('data', (data) => {
-            // console.log(`stderr: ${data}`);
-            errors.push(data)
-        });
-
-        // TODO: errors on stderr right now aren't actually problematic; 
-        //       improve this error handling
-        downloadProc.on('close', (code) => {
-            console.log(`exit code: ${code}`);
-            if (errors) {
-                console.log(errors.toString());
-                // reject({code, errors});
-                if (code == 0) {
-                    console.log('code is 0');
-                    loadLinker();
-                    resolve({code});
-                }
-            }
-            else {
-                loadLinker();
-                resolve({code});
-            }
-        });
-
-
-
-    });
-}
-
-function removePreviousOverlay () {
-    const { exec } = require('child_process')
-
-    let cmd = './' + downloadScriptPath + '/overlaymgr remove ' + previousProjectName;
-    exec(cmd, execCB);
-    cmd = './' + downloadScriptPath + '/drivermgr remove ' + previousProjectName;
-    exec(cmd, execCB);
-}
-
-loadLinker();
-
-const execCB = function (error, stdout, stderr) {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-}
-const progressCB = function (error, stdout, stderr) {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-    console.log('progress receipt: ' + stdout)
-    progress = JSON.parse(stdout.substring(1))
-    // if (stdout.charAt(0) == 'j') {
-    // }
-    // else if (stdout.charAt(0) == 'c') {
-    //     console.log('done!')
-    // }
-}
 
 exports.setCommandRequest = function (req, res) {
-    // console.log('New Command Received.');
     let body = [];
 
-    // console.log('New Command Received.');
     req.on('data', function (chunk) {
         body.push(chunk);
     });
 
-    // console.log('New Command Received.');
     req.on('end', function () {
 
-    // console.log('New Command Received.');
         try {
-            // console.log('asdf');
             postBody = JSON.parse(body);
-            // console.log('asdf');
             CommandObject = postBody;
-            // console.log('asdf');
             
-
-            // console.log(CommandObject)
-
-            // var majorNumber = findMajorNumber(CommandObject.module);
-            // console.log(majorNumber);
-            // var echoFile = findEchoFileFromLinker(CommandObject);
-            // console.log(echoFile);
-            //console.log('\nExpected echo path: ', echoFile)
-
-            // console.log('asdfasdf');
-            console.log(registerPaths);
-
+            // console.log(registerPaths);
             echoFile = registerPaths[CommandObject.module][CommandObject.link];
-
-            console.log(echoFile);
 
             var exec = require('child_process').exec;
 
-            // any unix based command
             var cmdToLaunch = "echo " + CommandObject.value + " > " + echoFile;
-            //var cmdToLaunch = "echo 12.00 > /sys/class/fe_bitcrusher_248/fe_bitcrusher_248/bits";
-
             console.log('Command: ', cmdToLaunch);
 
-            var app = exec(cmdToLaunch, execCB);
+            exec(cmdToLaunch, util.execCB);
 
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
@@ -252,7 +64,7 @@ exports.getUIRequest = function (req, res) {
         name = reqUrl.query.name
     }
 
-    const UIObject = getJsonFromFile(configPath + '/UI.json')
+    const UIObject = util.getJsonFromFile(configPath + '/UI.json')
 
     // console.log(JSON.stringify(UIObject));
 
@@ -268,7 +80,6 @@ exports.invalidRequest = function (req, res) {
 };
 
 exports.setDownloadRequest = function (req, res) {
-    //console.log('New Command Received.');
     let body = '';
 
     req.on('data', function (chunk) {
@@ -283,16 +94,19 @@ exports.setDownloadRequest = function (req, res) {
 
             // previous overlay needs to be removed before loading in a new one
             if (previousProjectName) {
-                removePreviousOverlay();
+                util.removePreviousOverlay(previousProjectName);
             }
 
             // reset progress back to 0 before downloading
             progress = '{"progress": 0, "status": ""}';
 
-            var downloadPromise = downloadInstallOverlay(CommandObject.downloadurl);
+            var downloadPromise = util.downloadInstallOverlay(CommandObject.downloadurl, configPath);
 
             downloadPromise.then((result) => {
                 console.log("success");
+
+                registerPaths = result.registerPaths;
+                console.log(registerPaths);
 
                 previousProjectName = CommandObject.projectname.replace('-', '_');
 
---
+++ utilities.js
@@ -0,0 +1,133 @@
+// 'use strict';
+const fs = require('fs');
+
+const deviceLocation = "/sys/class";
+const fe = "/fe_";
+const downloadScriptPath = "../utils"
+
+const execCB = function (error, stdout, stderr) {
+    if (error) {
+        console.error(`exec error: ${error}`);
+    }
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+}
+
+function createDevicePath (moduleName, majorNumber) {
+    const filepath = deviceLocation
+        + fe + moduleName.trim() + '_' + majorNumber
+        + fe + moduleName.trim() + '_' + majorNumber;
+    // console.log(`device path created: ${filepath}`);
+    return filepath;
+}
+
+function findMajorNumber (moduleName) {
+    // TODO: error handling; if module is not installed, the grep | cut command will return an empty string. We need to make sure that the returned major number is indeed an integer
+    const { execSync } = require('child_process');
+    const cmd = 'ls /sys/class | grep -P "fe_' + moduleName + '_\\d+" | cut -d _ -f 3';
+    const majorNumber = execSync(cmd);
+    // console.log(majorNumber.toString().trim());
+    return majorNumber.toString().trim();
+}
+
+const mapLinkNamesToFilepaths = function(LinkerObject) {
+    let registerPaths = {};
+     
+    // TODO: naming consistency between module and device (they refer to the same thing here)
+
+    for (const device in LinkerObject) {
+        if (LinkerObject.hasOwnProperty(device)) {
+            const deviceObject = LinkerObject[device];
+            const links = deviceObject['links'];
+
+            registerPaths[device] = {};
+
+            const majorNumber = findMajorNumber(device);
+            const devicePath = createDevicePath(device, majorNumber);
+
+            for (const link in links) {
+                if (links.hasOwnProperty(link)) {
+                    const registerName = links[link];
+
+                    // XXX: registerName is assumed to have a leading / in the linker json. This should always be the case, but having some error handling would be nice
+                    registerPaths[device][link] = devicePath + registerName;
+                }
+            }
+        }
+    }
+    console.log(registerPaths);
+    return registerPaths;
+}
+
+const getJsonFromFile = function (filepath) {
+    fs.open;
+    const objFromFile = fs.readFileSync(filepath,
+        { encoding: 'utf-8', flag: 'r' })
+    fs.close;
+
+    return JSON.parse(objFromFile);
+}
+
+const loadLinker = function(configPath) {
+    const LinkerObject = getJsonFromFile(configPath + '/Linker.json');
+    const registerPaths = mapLinkNamesToFilepaths(LinkerObject);
+    return registerPaths;
+}
+
+const downloadInstallOverlay = function(s3dir, configPath) {
+    return new Promise((resolve, reject) => {
+
+        let errors = [];
+        let registerPaths = {};
+
+
+        const { spawn } = require("child_process");
+        let downloadProc = spawn('python3', [downloadScriptPath + '/aws_overlay_installer.py', '-e', 'http://127.0.0.1:3355/set-download-progress', '-b', 'nih-demos', '-d', s3dir]);
+
+        downloadProc.stdout.on('data', (data) => {
+            console.log(`stdout: ${data}`);
+        });
+
+        downloadProc.stderr.on('data', (data) => {
+            console.log(`stderr: ${data}`);
+            errors.push(data)
+        });
+
+        // TODO: errors on stderr right now aren't actually problematic; 
+        //       improve this error handling
+        downloadProc.on('close', (code) => {
+            console.log(`exit code: ${code}`);
+            if (errors) {
+                // console.log(errors.toString());
+                // reject({code, errors});
+                if (code == 0) {
+                    registerPaths = loadLinker(configPath);
+                    resolve({code, registerPaths});
+                }
+                else {
+                    reject({code});
+                }
+            }
+            else {
+                registerPaths = loadLinker(configPath);
+                resolve({code, registerPaths});
+            }
+        });
+    });
+}
+
+const removePreviousOverlay = function(previousProjectName) {
+    const { exec } = require('child_process')
+
+    let cmd = './' + downloadScriptPath + '/overlaymgr remove ' + previousProjectName;
+    exec(cmd, execCB);
+    cmd = './' + downloadScriptPath + '/drivermgr remove ' + previousProjectName;
+    exec(cmd, execCB);
+}
+
+
+exports.execCB = execCB;
+exports.loadLinker = loadLinker;
+exports.getJsonFromFile = getJsonFromFile;
+exports.downloadInstallOverlay = downloadInstallOverlay;
+exports.removePreviousOverlay = removePreviousOverlay;
