0c4b743bb0271a32e1e76171e969497d517f8626
Trevor Vannoy <vannoy@flatearthinc.com>
2020-07-17 09:53:17 -0600

    fix ip/hostname so it doesn't show as undefined

--- EndpointServer.js
+++ EndpointServer.js
@@ -3,14 +3,14 @@ var networkInterfaces = os.networkInterfaces( );
 
 console.log( networkInterfaces );
 
-const hostname = networkInterfaces.eth0.address;
+const ip = networkInterfaces.eth0[0].address;
 const port = 3355;
 
 const server = require('./EndpointController.js');
 const service = require('./EndpointService.js');
 service.Init();
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, ip, () => {
+    console.log(`Server running at http://${ip}:${port}/`);
 });
 
 
