b3604dcfb60a3f6e1783c1c6724cf299ba225bb7
Trevor Vannoy <vannoy@flatearthinc.com>
2020-07-17 10:59:51 -0600

    change put to post

--- EndpointController.js
+++ EndpointController.js
@@ -8,19 +8,19 @@ module.exports = http.createServer((request, result) => {
     console.log('Request Type:' + request.method +
         ' Endpoint: ' + reqUrl.pathname);
 
-    if (reqUrl.pathname == '/model-data' && request.method === 'PUT') {
+    if (reqUrl.pathname == '/model-data' && request.method === 'POST') {
         service.setCommandRequest(request, result);
     }
     else if (reqUrl.pathname == '/model-data' && request.method === 'GET') {
         service.getModelData(request, result);
     }
-    else if (reqUrl.pathname == '/configuration' && request.method === 'PUT') {
+    else if (reqUrl.pathname == '/configuration' && request.method === 'POST') {
         service.setConfiguration(request, result);
     }
     else if (reqUrl.pathname == '/configuration' && request.method === 'GET') {
         service.getConfiguration(request, result); 
     }
-    else if(reqUrl.pathname == '/download' && request.method === 'PUT') {
+    else if(reqUrl.pathname == '/download' && request.method === 'POST') {
         service.setDownloadRequest(request, result); 
     } 
     else {
